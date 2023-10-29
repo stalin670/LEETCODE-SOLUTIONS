76deb3d9878b1b975465037a333375d7e41095f4
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-10-29 15:46:30 +0530

    Time: 2 ms (53.09%), Space: 6.8 MB (8.64%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int ans=0;
+        while(pow((minutesToTest/minutesToDie)+1,ans)<buckets){
+            ans++;
+        }
+        return ans;
+    }
+};
