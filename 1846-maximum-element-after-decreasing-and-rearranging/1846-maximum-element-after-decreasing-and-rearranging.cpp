c1fbdb4f0e1b45281abca01530d815e4d9177b04
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-11-15 12:57:41 +0530

    Time: 75 ms (75.00%), Space: 51.5 MB (96.27%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,8 +4,7 @@ public:
         sort(arr.begin(),arr.end());
         if(arr[0]!=1) arr[0]=1;
         for(int i=1;i<arr.size();i++){
-            int diff=arr[i]-arr[i-1];
-            if(diff<=1) continue;
+            if(arr[i]-arr[i-1]<=1) continue;
             arr[i]=arr[i-1]+1;
         }
         return arr[arr.size()-1];
