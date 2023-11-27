779351e8486f5576fde355e2b753e8274bc7742a
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-11-27 21:51:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (57.24%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        // ITERATE OVER ALL BITS 0 TO 31
+        // int ans=0;
+        // for(int i=0;i<32;i++){
+        //     int mask=1<<i;
+        //     if(mask&n) ans++;
+        // }
+        // return ans;
+        
+        // ONE OTHER WAY TO REMOVE EVERY SET BIT 
         int ans=0;
-        for(int i=0;i<32;i++){
-            int mask=1<<i;
-            if(mask&n) ans++;
+        while(n){
+            ans++;
+            n=n&(n-1);
         }
         return ans;
     }
