9f39aa993975bf5cfab979abb6ba42c29aa88996
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-11-27 22:01:52 +0530

    Time: 13 ms (6.06%), Space: 8.1 MB (99.33%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         // CHECK FOR ALL BIT 0 TO 31TH FOR ALL N
-        vector<int> ans;
+        // vector<int> ans;
+        // for(int i=0;i<=n;i++){
+        //     int t=0;
+        //     int num=i;
+        //     while(num){
+        //         t++;
+        //         num=num&(num-1);
+        //     }
+        //     ans.push_back(t);
+        // }
+        // return ans;
+        
+        vector<int> ans(n+1,0);
         for(int i=0;i<=n;i++){
-            int t=0;
-            int num=i;
-            while(num){
-                t++;
-                num=num&(num-1);
+            for(int j=0;j<32;j++){
+                int mask=1<<j;
+                if(i&mask) ans[i]++;
             }
-            ans.push_back(t);
         }
         return ans;
     }
