76de621ccb38475a26cddcafd0f5116d63ce75ef
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2023-11-27 22:12:27 +0530

    Time: 7 ms (26.63%), Space: 8.2 MB (63.97%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         // CHECK FOR ALL BIT 0 TO 31TH FOR ALL N
-        vector<int> ans(n+1,0);
-        for(int i=0;i<=n;i++){
-            int num=i;
-            while(num){
-                ans[i]++;
-                num=num&(num-1);
-            }
-        }
-        return ans;
+        // vector<int> ans(n+1,0);
+        // for(int i=0;i<=n;i++){
+        //     int num=i;
+        //     while(num){
+        //         ans[i]++;
+        //         num=num&(num-1);
+        //     }
+        // }
+        // return ans;
         
+        // USING MASK
         // vector<int> ans(n+1,0);
         // for(int i=0;i<=n;i++){
         //     for(int j=0;j<32;j++){
@@ -20,5 +21,14 @@ public:
         //     }
         // }
         // return ans;
+        
+        // DP
+        vector<int> dp(n+1,0);
+        int offset=1;
+        for(int i=1;i<=n;i++){
+            if(2*offset == i) offset=i;
+            dp[i]=1+dp[i-offset];
+        }
+        return dp;
     }
 };
