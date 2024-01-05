6d07e38c581d365ea411fbed471f9ad5d898edd7
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2024-01-05 20:49:58 +0530

    Time: 7 ms (86.91%), Space: 10.7 MB (92.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    int f(int ind,int prev,vector<int> arr,vector<vector<int>>& dp){
+        if(ind==size(arr)) return 0;
+        if(dp[ind][1+prev]!=-1) return dp[ind][prev];
+        
+        int notPick = 0+f(ind+1,prev,arr,dp);
+        int pick=0;
+        
+        if(prev==-1 || arr[ind]>arr[1+prev]){
+            pick = 1+f(ind+1,ind,arr,dp);
+        }
+        
+        return dp[ind][1+prev]=max(pick,notPick);
+    }
+    
     int lengthOfLIS(vector<int>& arr) {
         int n=arr.size();
         vector<int> t;
@@ -14,5 +28,8 @@ public:
             }
         }
         return t.size();
+        
+        vector<vector<int>> dp(size(arr),vector<int>(size(arr)+1,-1));
+        return f(0,-1,arr,dp);
     }
 };
