f63f3e3bffeab90ff4a2cdef2e0f629ed6943666
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2024-02-09 19:49:55 +0530

    Time: 7 ms (99.68%), Space: 11.4 MB (31.82%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -7,10 +7,8 @@ public:
         vector<int> hash(n,1);
         
         for(int i=0; i<=n-1; i++){
-            
             hash[i] = i; 
             for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-                
                 if(arr[i]%arr[prev_index]==0 && 1 + dp[prev_index] > dp[i]){
                     dp[i] = 1 + dp[prev_index];
                     hash[i] = prev_index;
