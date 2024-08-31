65038889d524953c778951c09e80d52b103fd64e
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-09-01 00:05:49 +0530

    Time: 18 ms (10.00%), Space: 14.4 MB (10.00%) - LeetHub

---
+++ 3271-hash-divided-string/3271-hash-divided-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ll long long
+    string stringHash(string s, int k) {
+        map<ll, char> mp;
+        for(char x = 'a', i = 0; x <= 'z'; x++, i++) {
+            mp[i] = x;
+        }
+        ll n = s.size();
+        ll temp_sum = 0;
+        string ans = "";
+        ll curr_len = 0;
+        for(ll i = 0; i < n; i++) {
+            temp_sum += (s[i] - 'a');
+            // cout << s[i] - 'a' << endl;
+            curr_len++;
+            if(k == curr_len) {
+                ans.push_back(mp[temp_sum % 26]);
+                temp_sum = 0;
+                curr_len = 0;
+            }
+            // cout << temp_sum << " ";
+        }
+        
+        return ans;
+    }
+};
