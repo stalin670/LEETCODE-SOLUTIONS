440771dff16446cd55738c8119a271f1f017b000
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-09-05 13:52:30 +0530

    Time: 57 ms (58.07%), Space: 53.8 MB (45.22%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,6 +4,7 @@ public:
     int firstMissingPositive(vector<int>& arr) {
         ll n = arr.size();
         bool have_one = false;
+        // Mark those value 1 which are negatives or greater than n
         for(ll i = 0; i < n; i++) {
             if(arr[i] == 1) {
                 have_one = true;
@@ -12,16 +13,19 @@ public:
                 arr[i] = 1;
             }
         }
-        
+        // If 1 is not present in the array then its the answer
         if(!have_one)
             return 1;
         
+        // Traverse the array and mark the current val as an index negative
         for(ll i = 0; i < n; i++) {
             ll val = abs(arr[i]);
             if(arr[val - 1] > 0) {
                 arr[val - 1] = -arr[val - 1];
             }
         }
+        
+        // Traverse the array and Check which index val is positive , that is the answer
         for(ll i = 0; i < n; i++) {
             if(arr[i] > 0)
                 return i + 1;
