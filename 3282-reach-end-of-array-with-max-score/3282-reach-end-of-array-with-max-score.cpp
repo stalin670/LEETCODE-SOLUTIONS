955903654c71e8fecdc1811170d88157e2c34621
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-09-08 17:49:31 +0530

    Time: 214 ms (20.00%), Space: 171.2 MB (100.00%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     #define ll long long
     long long findMaximumScore(vector<int>& nums) {
-        vector<pair<ll, ll>> arr;
-        arr.push_back({0, nums[0]});
-        ll ind = 0;
-        for(ll i = 1; i < nums.size(); i++) {
-            if(nums[i] > arr[ind].second) {
-                arr.push_back({i, nums[i]});
-                ind++;
+        ll n = nums.size();
+        ll ans = 0;
+        ll lastInd = 0;
+        for(ll i = 1; i < n; i++) {
+            if(nums[i] > nums[lastInd]) {
+                ans += (i - lastInd) * nums[lastInd];
+                lastInd = i;
             }
         }
-        if(arr.back().first != nums.size() - 1) {
-            arr.push_back({nums.size() - 1, nums[nums.size() - 1]});
+        if(lastInd != n - 1) {
+            ans += (n - 1 - lastInd) * nums[lastInd];
         }
-        
-        ll ans = 0;
-        for(ll i = 1; i < arr.size(); i++) {
-            ans += (arr[i].first - arr[i - 1].first) * arr[i - 1].second;
-        }
-        
         return ans;
     }
 };
