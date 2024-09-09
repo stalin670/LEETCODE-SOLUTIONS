9465962424f49ba4a0d81495411f8e7aa1249341
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-09-10 00:30:57 +0530

    Time: 151 ms (88.30%), Space: 130.6 MB (65.45%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    #define ll long long
+    vector<vector<int>> spiralMatrix(int n, int m, ListNode* head) {
+        vector<vector<int>> ans(n, vector<int>(m, - 1));
+        // ll i = 0, j = 0;
+        // bool right = true, down = false, up = false, left = false;
+        ll last = 1;
+        ll top = 0, bottom = n - 1, left = 0, right = m - 1;
+ 
+        int index = 0;
+
+        while (head != NULL) {
+
+            if (left > right)
+                break;
+
+            // print top row
+            for (int i = left; i <= right and head != NULL; i++) {
+                ans[top][i] = head -> val;
+                head = head -> next;
+            }
+            top++;
+
+            if (top > bottom)
+                break;
+
+            // print right column
+            for (int i = top; i <= bottom and head != NULL; i++) {
+                ans[i][right] = head -> val;
+                head = head -> next;
+            }
+            right--;
+
+            if (left > right)
+                break;
+
+            // print bottom row
+            for (int i = right; i >= left and head != NULL; i--) {
+                ans[bottom][i] = head -> val;
+                head = head -> next;
+            }
+            bottom--;
+
+            if (top > bottom)
+                break;
+
+            // print left column
+            for (int i = bottom; i >= top and head != NULL; i--) {
+                ans[i][left] = head -> val;
+                head = head -> next;
+            }
+            left++;
+        }
+        return ans;
+    }
+};
