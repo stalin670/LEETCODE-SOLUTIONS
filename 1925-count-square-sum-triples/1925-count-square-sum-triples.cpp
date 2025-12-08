f3f01adc4de340fb062488c82ccb7fbfaaddcd6f
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-12-08 11:58:44 +0530

    Time: 42 ms (30.09%), Space: 7.8 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -3,10 +3,10 @@ public:
     int countTriples(int n) {
         int ans = 0;
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                for(int k = 1; k <= n; k++) {
-                    if(i != j and j != k and k != i and i * i + j * j == k * k)
-                        ans++;
+            for(int j = i + 1; j <= n; j++) {
+                for(int k = j + 1; k <= n; k++) {
+                    if(i * i + j * j == k * k)
+                        ans += 2;
                 }
             }
         }
