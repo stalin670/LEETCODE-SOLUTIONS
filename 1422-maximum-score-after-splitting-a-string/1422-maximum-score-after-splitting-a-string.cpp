3080176bcd4d27c37b7d9a4bc95d5b9fda011680
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-12-19 14:13:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (59.62%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int cnt=0;
-        for(auto it:s) cnt+=(it=='1');
-        int ans=0,x=0;
-        for(int i=0;i<size(s)-1;i++){
-            x+=(s[i]=='0');
-            cnt-=(s[i]=='1');
-            ans=max(ans,x+cnt);
+        int n = s.size();
+        int pref0[n];
+        int suff1[n];
+        pref0[0] = (s[0] == '0' ? 1 : 0);
+        suff1[n - 1] = (s[n - 1] == '1' ? 1 : 0);
+        for(int i = 1; i < n; i++) {
+            pref0[i] = pref0[i - 1] + (s[i] == '0' ? 1 : 0);
         }
-        return ans;
+
+        for(int i = n - 2; i >= 0; i--) {
+            suff1[i] = suff1[i + 1] + (s[i] == '1' ? 1 : 0);
+        }
+        int maxScore = 0;
+        for(int i = 0; i < n - 1; i++) {
+            maxScore = max(maxScore, pref0[i] + suff1[i + 1]);
+        }
+        return maxScore;
     }
 };
