fe662c61cf501fcb7d4879d3e37ce91d15fb698a
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-12-30 14:26:58 +0530

    Time: 8 ms (40.41%), Space: 20.3 MB (75.12%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.c
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.c
@@ -1,31 +1,29 @@
 #define ll long long
 
-ll min(ll a, ll b) {
-    if(a < b) return a;
-    return b;
-}
 ll max(ll a, ll b) {
     if(a > b) return a;
     return b;
 }
 
-long long maximumScore(int* nums, int n) {
+ll min(ll a, ll b) {
+    if(a < b) return a;
+    return b;
+}
+
+long long maximumScore(int* arr, int n) {
     ll prefSum[n];
     ll suffMin[n];
-    prefSum[0] = nums[0];
-    suffMin[n - 1] = nums[n - 1];
-
-    for(int i = 1; i < n; i++) {
-        prefSum[i] = prefSum[i - 1] + nums[i];
+    for(ll i = 0; i < n; i++) {
+        if(i == 0) prefSum[0] = arr[0];
+        else prefSum[i] = prefSum[i - 1] + arr[i];
     }
-
-    for(int i = n - 2; i >= 0; i--) {
-        suffMin[i] = min(suffMin[i + 1], nums[i]);
+    for(ll i = n - 1; i >= 0; i--) {
+        if(i == n - 1) suffMin[n - 1] = arr[n - 1];
+        else suffMin[i] = min(suffMin[i + 1], arr[i]);
     }
-
-    ll ans = -1000000000000000;
-    for(int i = 0; i < n - 1; i++) {
-        ans = max(ans, prefSum[i] - suffMin[i + 1]);
+    ll score = -10000000000000000;
+    for(ll i = 0; i < n - 1; i++) {
+        score = max(score, prefSum[i] - suffMin[i + 1]);
     }
-    return ans;
+    return score;
 }
