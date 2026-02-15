b5e284783a0aa67aeeeae037ff3fbf41e6405225
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2026-02-15 23:58:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (59.48%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,69 +1,25 @@
 class Solution {
 public:
-    // Simple Addition from Back side
     string addBinary(string a, string b) {
-        string s = "";
-        int i = a.size() - 1, j = b.size() - 1;
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+
         int carry = 0;
-        while(i >= 0 and j >= 0) {
-            int x = a[i] - '0';
-            int y = b[j] - '0';
-            int z = x + y + carry;
-            if(z > 1) {
-                carry = 1;
-                if(z == 2) 
-                    s += '0';
-                else
-                    s += '1';
-            }
-            else {
-                carry = 0;
-                if(z == 1) 
-                    s += '1';
-                else
-                    s += '0';
-            }
-            i--;
-            j--;
+        int n = a.size(), m = b.size();
+        string s = "";
+        int i = 0, j = 0;
+        while(i < n || j < m) {
+            int sum = carry;
+
+            if(i < n) sum += (a[i++] - '0');
+            if(j < m) sum += (b[j++] - '0');
+
+            s += char((sum % 2) + '0');
+            carry = sum / 2;
         }
-        
-        while(i >= 0) {
-            int x = a[i] - '0';
-            int z = x + carry;
-            if(z > 1) {
-                carry = 1;
-                s += '0';
-            }
-            else {
-                carry = 0;
-                if(z == 1) 
-                    s += '1';
-                else
-                    s += '0';
-            }
-            i--;
-        }
-        while(j >= 0) {
-            int y = b[j] - '0';
-            int z = y + carry;
-            if(z > 1) {
-                carry = 1;
-                s += '0';
-            }
-            else {
-                carry = 0;
-                if(z == 1) 
-                    s += '1';
-                else
-                    s += '0';
-            }
-            j--;
-        }
-        
-        if(carry) {
-            s += '1';
-        }
-        
+
+        if(carry) s += '1';
+
         reverse(s.begin(), s.end());
         return s;
     }
